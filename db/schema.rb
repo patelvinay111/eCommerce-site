87221e6fe6c2251b9b566f9a2f8c58ba1961251a
jchee <jasonchee@live.com>
2016-02-16 15:36:32 -0800

    with items database and new linkings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216225756) do
+ActiveRecord::Schema.define(version: 20160216232642) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.float    "price"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
